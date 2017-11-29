9c8d8612f2dec7de8743ba6d685cce0befe0ca80
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-29 10:52:21 -0500

    Parcial

---
+++ src/main/java/Controller/SedeController.java
@@ -0,0 +1,18 @@
+/*
+ *  PARCIAL TERCER CORTE
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
+ */
+
+package Controller;
+
+import VO.*;
+
+/**
+ *
+ * @author BRIAN
+ */
+public class SedeController {
+    
+}
