1beeac724cecd71703560a200a3c14f4cd90bba9
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-29 11:54:31 -0500

    Parcial

--- src/main/java/dao/ApiarioDAO.java
+++ src/main/java/dao/ApiarioDAO.java
@@ -7,10 +7,109 @@
 
 package dao;
 
-/**
- *
- * @author BRIAN
- */
-public class ApiarioDAO {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import util.DbUtil;
+import vo.Apiario;
+
+
+public class ApiarioDAO
+{
+    private Connection connection;
+
+    public ApiarioDAO()
+    {
+        connection = DbUtil.getConnection();
+    }
+
+    public void addUser(Apiario user)
+    {
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into sede(idSede) values (?)");
+            preparedStatement.setInt(1, user.getIdSede());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int idApiario)
+    {
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from sede where idApiario=?");
+            preparedStatement.setInt(1, idApiario);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(Apiario user)
+    {
+        try 
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("update sede set idSede=?" + "where idApiario=?");
+            preparedStatement.setInt(1, user.getIdSede());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Apiario> getAllUsers()
+    {
+        List<Apiario> users = new ArrayList<Apiario>();
+        try
+        {
+            System.out.println("Llegue hasta aca");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from sede");
+            while (rs.next())
+            {
+                Apiario user = new Apiario();
+                user.setIdApiario(rs.getInt("idApiario"));
+                user.setIdSede(rs.getInt("idApiario"));
+                users.add(user);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public Apiario getUserById(int idApiario)
+    {
+        Apiario user = new Apiario();
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from sede where idApiario=?");
+            preparedStatement.setInt(1, idApiario);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next())
+            {
+                user.setIdApiario(rs.getInt("idApiario"));
+                user.setIdSede(rs.getInt("idApiario"));
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
--- src/main/java/dao/SedeDAO.java
+++ src/main/java/dao/SedeDAO.java
@@ -7,10 +7,108 @@
 
 package dao;
 
-/**
- *
- * @author BRIAN
- */
-public class SedeDAO {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import util.DbUtil;
+import vo.Sede;
+
+
+public class SedeDAO
+{
+    private Connection connection;
+
+    public SedeDAO()
+    {
+        connection = DbUtil.getConnection();
+    }
+
+    public void addUser(Sede user)
+    {
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into sede(idSede) values (?)");
+            preparedStatement.setString(1, user.getNombre());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int idApiario)
+    {
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from sede where idApiario=?");
+            preparedStatement.setInt(1, idApiario);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(Sede user)
+    {
+        try 
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("update sede set idSede=?" + "where idApiario=?");
+            preparedStatement.setString(1, user.getNombre());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Sede> getAllUsers()
+    {
+        List<Sede> users = new ArrayList<Sede>();
+        
+        try
+        {
+            System.out.println("Llegue hasta aca");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from sede");
+            while (rs.next())
+            {
+                Sede user = new Sede();
+                user.setNombre(rs.getString("idApiario"));
+                users.add(user);
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public Sede getUserById(int idApiario)
+    {
+        Sede user = new Sede();
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from sede where idApiario=?");
+            preparedStatement.setInt(1, idApiario);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next())
+            {
+                user.setNombre(rs.getString("idApiario"));
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
