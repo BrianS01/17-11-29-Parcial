09dd5c0bae2524ba0759071d285743225470f3c9
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-29 10:48:37 -0500

    Parcial

---
+++ src/main/java/VO/Recolector.java
@@ -0,0 +1,16 @@
+/*
+ *  PARCIAL TERCER CORTE
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
+ */
+
+package VO;
+
+/**
+ *
+ * @author BRIAN
+ */
+public class Recolector {
+    
+}
