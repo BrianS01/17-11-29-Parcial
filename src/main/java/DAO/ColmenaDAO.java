1e614b034bd27ee60f0b92364fab402f3b9b4dc2
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-29 10:50:17 -0500

    Parcial

---
+++ src/main/java/DAO/ColmenaDAO.java
@@ -0,0 +1,18 @@
+/*
+ *  PARCIAL TERCER CORTE
+ *      Author ::: Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
+ */
+
+package DAO;
+
+import VO.*;
+
+/**
+ *
+ * @author BRIAN
+ */
+public class ColmenaDAO {
+    
+}
